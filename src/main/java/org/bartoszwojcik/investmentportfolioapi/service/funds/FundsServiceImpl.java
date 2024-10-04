4de5417cdfbf00db4e25955361857b2dffcaba81
Banderan <bartoszwojcik2004@gmail.com>
2024-10-04 16:08:08 +0200

    add conversion other values to PLN

--- src/main/java/org/bartoszwojcik/investmentportfolioapi/service/funds/FundsServiceImpl.java
+++ src/main/java/org/bartoszwojcik/investmentportfolioapi/service/funds/FundsServiceImpl.java
@@ -148,8 +148,6 @@ public class FundsServiceImpl implements FundsService {
 
     @Override
     public String withdrawal(User user, BigDecimal amount) {
-        String email = user.getEmail();
-        // if user.cash < amount - exception
         if (user.getCash().compareTo(amount) < 0) {
             throw new PaymentException(
                     "You don't have enough money to withdraw " + amount);
@@ -157,6 +155,7 @@ public class FundsServiceImpl implements FundsService {
         user.setCash(user.getCash().subtract(amount));
         userRepository.save(user);
 
+        String email = user.getEmail();
         return notificationService.userWannaWithdrawal(
                 email, amount
         );
