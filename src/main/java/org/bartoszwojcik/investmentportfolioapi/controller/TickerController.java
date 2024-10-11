74fb3b4a42afd4c2b9c0e16bf7022323a3d3188b
Banderan <bartoszwojcik2004@gmail.com>
2024-10-11 15:33:00 +0200

    rename Stock -> Ticker for better navigation

--- src/main/java/org/bartoszwojcik/investmentportfolioapi/controller/TickerController.java
+++ src/main/java/org/bartoszwojcik/investmentportfolioapi/controller/TickerController.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.bartoszwojcik.investmentportfolioapi.dto.stock.external.StockDto;
-import org.bartoszwojcik.investmentportfolioapi.service.stock.StockService;
+import org.bartoszwojcik.investmentportfolioapi.service.ticker.TickerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,32 +15,32 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/stocks")
+@RequestMapping("/tickers")
 @RequiredArgsConstructor
-public class StockController {
-    private final StockService stockService;
+public class TickerController {
+    private final TickerService stockService;
 
     @Operation(summary = "get available stocks in this app")
     @GetMapping("/all")
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.OK)
-    public List<StockDto> getStocks() {
-        return stockService.getStocks();
+    public List<StockDto> getTicker() {
+        return stockService.getTickers();
     }
 
     @Operation(summary = "add new stock to our app")
     @PostMapping("/add-new")
     @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
-    public String addStock(@RequestParam String stockSymbol) {
-        return stockService.addStock(stockSymbol);
+    public String addTicker(@RequestParam String stockSymbol) {
+        return stockService.addTicker(stockSymbol);
     }
 
     @Operation(summary = "add new stock to our app without checking if it is correct ticker")
     @PostMapping("/add-new/force")
     @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
-    public String addStockWithoutChecking(@RequestParam String stockSymbol) {
-        return stockService.addStock(stockSymbol, true);
+    public String addTickerWithoutChecking(@RequestParam String stockSymbol) {
+        return stockService.addTicker(stockSymbol, true);
     }
 }
--- src/main/java/org/bartoszwojcik/investmentportfolioapi/service/ticker/TickerServiceImpl.java
+++ src/main/java/org/bartoszwojcik/investmentportfolioapi/service/ticker/TickerServiceImpl.java
@@ -1,4 +1,4 @@
-package org.bartoszwojcik.investmentportfolioapi.service.stock;
+package org.bartoszwojcik.investmentportfolioapi.service.ticker;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -15,27 +15,31 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class StockServiceImpl implements StockService {
+public class TickerServiceImpl implements TickerService {
     private final SerpApiConfig serpApiConfig;
     private final StockRepository stockRepository;
     private final StockMapper stockMapper;
     private final CurrencyApiClientConfig currencyApiClientConfig;
 
     @Override
-    public List<StockDto> getStocks() {
+    public List<StockDto> getTickers() {
         return stockRepository.findAll().stream()
                 .map(stockMapper::toStockDto)
                 .toList();
     }
 
     @Override
-    public String addStock(String stockSymbol) {
+    public String addTicker(String stockSymbol) {
         GooglePageForStocksWrapper companyInformation = serpApiConfig
                 .getCompanyInformation(stockSymbol);
 
         Stock stock = new Stock();
         stock.setStockSymbol(companyInformation.getAnswerBox().getStock());
         stockRepository.save(stock);
+        return getStockInformation(companyInformation);
+    }
+
+    private String getStockInformation(GooglePageForStocksWrapper companyInformation) {
         String companyName = companyInformation.getAnswerBox().getTitle();
         String companyTicker = companyInformation.getAnswerBox().getStock();
         BigDecimal companyPrice = companyInformation.getAnswerBox().getPrice();
@@ -48,13 +52,14 @@ public class StockServiceImpl implements StockService {
             value = BigDecimal.valueOf(1);
         }
         BigDecimal priceInPln = companyPrice.multiply(value);
-        return companyName + " with ticker: " + companyTicker
+        String stockInfo = companyName + " with ticker: " + companyTicker
                 + " and price: " + companyPrice + " " + currency + " added to database"
                 + " in PLN it is: " + priceInPln;
+        return stockInfo;
     }
 
     @Override
-    public String addStock(String stockSymbol, boolean force) {
+    public String addTicker(String stockSymbol, boolean force) {
         Stock stock = new Stock();
         stock.setStockSymbol(stockSymbol);
         if (force) {
